c0d08f09f9c53b6cb02ec0dbc3fe3ed734231cf0
Fakhri17 <fakhricoffe@gmail.com>
2021-10-31 14:38:18 +0700

    first commit

---
+++ operator.py
@@ -0,0 +1,45 @@
+#Operator Arimatika
+# Terdiri dari: 
+# Penjumlahan (+), Pengurangan (-), Perkailian (*)
+# Pembagian (/), Sisa Bagi (%), Pangkat (**), Pembagian Bulat (//)
+
+#(+)
+print (20 + 22)
+harga1 = 15
+harga2 = 20
+total = harga1 + harga2
+print (total)	
+
+#(-)
+hutang = 100000
+bayar = 50000
+sisaHutang = hutang - bayar
+print (sisaHutang)
+
+#(*)
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
+
+#(/)
+uang = 50000
+anak = 5
+uangSaku = uang / anak
+print (" Uang saku tiap anak adalah ", int(uangSaku))
+
+
+#(%)
+bilangan1 = 14
+bilangan2 = 5
+hasil = bilangan1 % bilangan2
+print("Sisa bagi dari bilangan ", bilangan1, " dan ", bilangan2, " adalah ", hasil)
+
+#(**)
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print(hasilPangkat)
+
+#Pembagian
+print(10//3) 
---
+++ variabel.py
@@ -0,0 +1,30 @@
+#proses memasukan data ke dalam variabel
+nama = "Fakhri Alauddin"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel  dapat diubah
+umur = 20               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "dua puluh 5" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Fakhri"
+namaBelakang = "Alauddin"
+nama = namaDepan + " " + namaBelakang
+umur = 20
+hobi = "Ngoding"
+print("Biodata", nama, umur, hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye" 
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
